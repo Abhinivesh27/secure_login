0ab7682b383ef48c555c113526a0fca7a69a66d7
Abhinivesh27 <77716914+Abhinivesh27@users.noreply.github.com>
2021-09-11 07:29:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function flag() {
+    var data = document.getElementById("password");
+    if (data.value == "1dc38d462b3de984a4fe5a52f7b74337") {
+        alert("Flag captured");
+    } else {
+        alert("Wrong Password, Try again !!");
+    }
+}
